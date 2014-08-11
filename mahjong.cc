224dfb7af6a9649d72c28a355c0d05bc69d53bdd
gudrodsson <gudrodsson@gmail.com>
2014-08-11 09:51:04 +0000

    struttura dati iniziale

---
+++ mahjong.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std ;
+
+struct tile {
+            bool    empty ;
+            bool    lock ;
+            char *  name ;
+            int     pos_x ;
+            int     pos_y ;
+            int     pos_z ;
+            }
+
+int main ()
+{
+
+    return 0 ;
+}
