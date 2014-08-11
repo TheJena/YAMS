ef65b0c55611dfd2726ed7443f3ad2dd85fe0dd8
gudrodsson@gmail.com <gudrodsson@gmail.com@b3031337-ebf3-032d-615f-2c29d1ed0f9c>
2014-08-11 10:56:50 +0000

    struttura dati iniziale

--- trunk/mahjong.cc
+++ trunk/mahjong.cc
@@ -6,11 +6,59 @@ struct tile {
             bool    empty ;
             bool    lock ;
             char *  name ;
-            int     pos_x ;
-            int     pos_y ;
-            int     pos_z ;
             }
 
+const int dim_X = 12 ;
+const int dim_Y = 8 ;
+const int dim_Z = 4 ;
+/*
+ * Funzione che crea uno spazio tridimensionale (x, y, z) di tessere.
+ */
+void create_cube ( tile *** cube )
+{
+    cube = new tile ** [dim_X] ;
+
+    for ( int x = 0 ; x < dim_X ; x++ )
+    {
+        cube[x] = new tile * [dim_Y] ;
+
+        for ( int y = 0 ; y < dim_Y ; y++ )
+            cube[x][y] = new tile [dim_Z] ;
+    }
+}
+
+void initialize_cube ( tile *** cube )
+{
+    for ( int x = 0 ; x < dim_X ; x++)
+        for ( int y = 0 ; y < dim_Y ; y++)
+            for ( int z = 0 ; z < dim_Z ; z++)
+            {
+                cube[x][y][z].empty = true ;
+                cube[x][y][z].lock  = true ;
+                cube[x][y][z].name  = NULL ;
+            }
+}
+
+void delete_cube ( tile *** cube )
+{
+    for ( int x = 0 ; x < dim_x ; x++ )
+    {
+        for ( int y = 0 ; y < dim_Y ; y++ )
+        {
+            for ( int z = 0 ; z < dim_Z ; z++ )
+            {
+                if ( cube[x][y][z].name != NULL )
+                    delete [] cube[x][y][z].name ;
+
+                delete [] cube[x][y] ;
+            }
+        }
+
+        delete [] cube[x] ;
+    }
+
+    delete [] cube ;
+}
 int main ()
 {
 
