fbc2d124595ac828bfedff0d287264ffdc995287
gudrodsson@gmail.com <gudrodsson@gmail.com@b3031337-ebf3-032d-615f-2c29d1ed0f9c>
2014-09-04 14:31:13 +0000

    totally_changed

--- trunk/mahjong.cc
+++ trunk/mahjong.cc
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <gtk/gtk.h>
+#include "data_structure.h"
+#include "initializations_operations.h"
+#include "gui.h"
+#include "io_file.h"
+
 using namespace std ;
-static GtkBuilder * builder ;
+
+static const char* F_GLADE             = "./gui.glade" ;
+
+
+
 int main ( int argc, char * argv[] )
 {
-/*                                    CONTROLLARE CON FILE HEADER
     gtk_init ( &argc, &argv) ;
     builder = gtk_builder_new () ;
-    gtk_builder_add_from_file ( builder, "./gui.glade", NULL ) ;
+    gtk_builder_add_from_file ( builder, F_GLADE, NULL ) ;
     gtk_builder_connect_signals(builder, NULL);
 
-    gtk_main () ;                                                       */
+    create_cube () ;
+    initialize_cube () ;
+
+    import_tiles_names () ;
+    fill_cube () ; /* nota: chiama gia la mix_cube()*/
+    check_cube () ;
+    unlocked[0] = NULL ;
+    refresh_unlocked () ; /* nota: chiama gia la sort_unlocked()*/
+
+
+    gtk_main () ;
     /*
      * non inserire nulla qui in mezzo.
      * questa parte viene eseguita quando si chiude la finestra.
