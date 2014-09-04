556d82004f91ebb24f693014f3d8f4a80d79e767
gudrodsson <gudrodsson@gmail.com>
2014-09-04 14:29:22 +0000

    totally_changed

--- data_structure.cc
+++ data_structure.cc
@@ -1,36 +1,25 @@
-#include <iostream>
+#include <cstdlib>
+#include "data_structure.h"
 
 using namespace std ;
 
-const int   TILES       = 144 ;
+const int TILES = 144 ;
+
+const int dim_X = 12 ;
+const int dim_Y = 8 ;
+const int dim_Z = 4 ;
+
 const int   SEEDVALUE   = 1 ;
 const int   WINDVALUE   = 3 ;
 const int   DRAGONVALUE = 5 ;
 const int   SEASONVALUE = 12 ;
 const int   FLOWERVALUE = 14 ;
-const char  F_TILES     = "./tiles_names.dat" ;
 
-enum layout { easy, medium, difficult } level ;
+layout level = medium ;
 
-enum computer { airhead, greedy, thoughtful } ai;
-
-struct tile {
-                bool    empty ;
-                bool    lock ;
-                int     num ;
-                int     value ;
-            } ;
+computer ai = greedy ;
 
 tile *** cube = NULL ;
 tile *    unlocked[TILES] ;
-struct string   {
-                    char * word ;
-                } ;
 
-string * name = NULL ;
-
-
-
-
-
-
+_string * name = NULL ;
